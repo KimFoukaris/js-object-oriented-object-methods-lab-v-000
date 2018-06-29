2a00e080c166e247879b5dec979d3df16b31cc5b
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-29 12:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function BoardMember (name, homeState, training) {
   this.homeState = homeState
   this.training = training
   this.veto = function() {
-    "No, I must diagree"
+    "No, I must diagree";
   }
 }
